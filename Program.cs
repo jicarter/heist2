c1581d186aa9fbe44cff8a9d2e34a6e8cd613ec6
Jordan Carter <jordan.carter.0023@gmail.com>
2021-03-16 14:15:32 -0500

    interfaces

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace heist2
 {
@@ -6,7 +8,49 @@ namespace heist2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        }
+        
+            public class Bank
+            {
+                 public int CashOnHand { get; set; }
+                 public int AlarmScore { get; set; }
+                 public int VaultScore { get; set; }
+                 public int SecurityGuardScore { get; set; }
+                 public bool IsSecure { get; set; }
+                 public void TotalSecurity()
+                 {
+                      if (CashOnHand <= 0 || AlarmScore <= 0 || VaultScore <= 0 || SecurityGuardScore <= 0)
+                      {
+                          IsSecure = false;
+                      }
+                 } 
+            }
+
+            public interface IRobber
+            {
+                public string Name { get; set; }
+                public int SkillLevel { get; set; }
+                public int PercentageCut { get; set; }
+
+                public void PerformSkill(Bank bank)
+                {
+
+                }
+
+            }
+
+            public class Hacker : IRobber 
+            {
+               public string Name { get; set; }
+               public int SkillLevel {get ; set; }
+               public int PercentageCut { get; set; }
+
+                public void PerformSkill(Bank bank)
+                {
+                    SkillLevel - Bank.AlarmScore
+                }
+            }
         }
     }
-}
+
